a8f782c0a12d4c1cb3110689249cc09bbe0ed2eb
Tommy Persaud <68301047+tpersaud27@users.noreply.github.com>
2022-06-30 13:48:01 -0400

    Updates

--- JavaScript/LinkedList/singlyLinkedList.js
+++ JavaScript/LinkedList/singlyLinkedList.js
@@ -12,10 +12,7 @@ class SinglyLinkedList {
     this.tail = null;
   }
 
-    insertAtBeginning(value) {
-      
-         
-  }
+  insertAtBeginning(value) {}
 
   insertAtEnd(value) {
     this.length++;
