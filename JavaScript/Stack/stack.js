1829b40510e9e200ea474e83f0a6a3629803a489
Tommy Persaud <tpersaud27@gmail.com>
2022-07-07 15:15:53 -0400

    Stack methods

--- JavaScript/Stack/stack.js
+++ JavaScript/Stack/stack.js
@@ -20,7 +20,7 @@ class Stack {
   // This returns the top element of the stack
   peek() {
     // Check if the stack is empty, return null if true
-    if (this.items.length === 0) {
+    if (this.items.length == 0) {
       return null;
     }
     // Return the top element of the stack
@@ -33,5 +33,49 @@ class Stack {
     return this.items.length === 0;
   }
 
-  //
+  // Returns the size of the stack
+  size() {
+    // We just return the size of the array / stack
+    return this.items.length;
+  }
+
+  // Adding a new element to the stack
+  push(element) {
+    // Add the new element to the stack
+    this.items.push(element);
+    // Let the top be the new element added
+    this.top = element;
+  }
+
+  // Removes the top element from the stack
+  pop() {
+    // Check if the stack is not empty
+    if (this.items.length !== 0) {
+      // Check if there is only one element in the stack
+      if (this.items.length === 1) {
+        // Let the top element be null because we are removing it
+        this.top = null;
+        // Return the removed element
+        return this.items.pop();
+      }
+      // Else we make top the second to last element
+      else {
+        // We getting the last second to last index
+        this.top = this.items[this.items.length - 2];
+        // Return the last element that we removed
+        return this.items.pop();
+      }
+    }
+    // If the stack doesnt exist return null
+    else {
+      return null;
+    }
+  }
 }
+
+// ----------------------------------------------------------------
+let myStack = new Stack();
+
+console.log("You have successfully created a Stack of size: " + myStack.size());
+console.log("Is stack empty? " + myStack.isEmpty());
+console.log("top: " + myStack.peek());
